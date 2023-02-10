976429b491761e50e7676f429db3407a0606cecb
wassim-kaouia <wassim.elkaouia@gmail.com>
2023-02-10 15:21:09 +0000

    first commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 8080;
 const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
-  res.send('Hello World from IBM Cloud Essentials!');
+  res.send('Hello World for NodeJs Dockerized application ! - Wassim Kaouia');
 })
 
 app.listen(port, host);
